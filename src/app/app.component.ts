6133ee1f0e3ff7f3b0de4785a6556c3db4d18ef6
Daniel Neto <daniel.jsilvan@gmail.com>
2023-11-27 11:42:08 -0300

    updating app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TesterComponent } from './tester/tester.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, TesterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
